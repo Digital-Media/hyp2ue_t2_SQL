a6a7e4a35f3d9173791150f1237462abd8478b9e
martinharrer <martin.harrer@fh-hagenberg.at>
2021-11-17 14:36:38 +0100

    initial file upload

---
+++ sql/login.sql
@@ -0,0 +1,11 @@
+-- part 1 DDL
+DROP SCHEMA IF EXISTS login;
+CREATE SCHEMA IF NOT EXISTS `login` DEFAULT CHARACTER SET utf8;
+USE login;
+-- place your solutions here
+
+
+
+-- end solution
+
+DROP SCHEMA login;
---
+++ sql/ue2.sql
@@ -0,0 +1,4 @@
+-- part 2 DML
+\. /var/www/html/code/onlineshop/src/onlineshop.sql
+use onlineshop;
+-- 2
