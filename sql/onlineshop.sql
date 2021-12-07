8819f69b91ebf66b4046c3755250f6a3ced2f0e7
Martin Harrer <18611980+martinharrer@users.noreply.github.com>
2021-12-07 12:37:20 +0100

    Update onlineshop.sql

--- sql/onlineshop.sql
+++ sql/onlineshop.sql
@@ -1,33 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 3.2.2.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 15, 2010 at 09:40 AM
--- Server version: 5.1.37
--- PHP Version: 5.2.10-2ubuntu6.4
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Datenbank: `onlineshop`
---
-DROP SCHEMA  IF EXISTS onlineshop;
-CREATE SCHEMA IF NOT EXISTS `onlineshop` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE SCHEMA IF NOT EXISTS `onlineshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `onlineshop`;
 
--- --------------------------------------------------------
---
--- Tabellenstruktur für Tabelle `address`
---
-
-DROP TABLE IF EXISTS `address`;
 CREATE TABLE IF NOT EXISTS `address` (
   `idaddress` bigint unsigned NOT NULL AUTO_INCREMENT,
   `address_line` varchar(100) NOT NULL,
@@ -38,21 +11,6 @@ CREATE TABLE IF NOT EXISTS `address` (
   KEY `cityfk` (`city_idcity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- RELATIONEN DER TABELLE `address`:
---   `city_idcity`
---       `city` -> `idcity`
---   `user_iduser`
---       `user` -> `iduser`
---
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `cart`
---
-
-DROP TABLE IF EXISTS `cart`;
 CREATE TABLE IF NOT EXISTS `cart` (
   `session_id` varchar(250) NOT NULL,
   `product_idproduct` bigint NOT NULL,
@@ -67,14 +25,6 @@ INSERT INTO `cart` (`session_id`, `product_idproduct`, `product_name`, `price`, 
 ('1', 5, 'Talgrundstück', 10000.00, 1),
 ('2', 5, 'Talgrundstück', 10000.00, 2);
 
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `city`
---
-
-DROP TABLE IF EXISTS `city`;
 CREATE TABLE IF NOT EXISTS `city` (
   `idcity` bigint unsigned NOT NULL AUTO_INCREMENT,
   `PLZ` varchar(10) NOT NULL,
@@ -84,13 +34,6 @@ CREATE TABLE IF NOT EXISTS `city` (
   KEY `countryfk` (`country_idcountry`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `country`
---
-
-DROP TABLE IF EXISTS `country`;
 CREATE TABLE IF NOT EXISTS `country` (
   `idcountry` bigint unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -98,21 +41,10 @@ CREATE TABLE IF NOT EXISTS `country` (
   PRIMARY KEY (`idcountry`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Daten der Tabelle `country`
---
-
 insert into country set name = 'Austria', ISOcode = 'AT';
 insert into country set name = 'Deutschland', ISOcode = 'DE';
 insert into country set name = 'Schweiz', ISOcode = 'CH';
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
   `idorders` bigint unsigned NOT NULL AUTO_INCREMENT,
   `user_iduser` bigint unsigned NOT NULL,
@@ -135,13 +67,6 @@ CREATE TABLE IF NOT EXISTS `orders` (
   KEY `user_fk` (`user_iduser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `order_item`
---
-
-DROP TABLE IF EXISTS `order_item`;
 CREATE TABLE IF NOT EXISTS `order_item` (
   `orders_idorders` bigint unsigned NOT NULL,
   `product_idproduct` bigint unsigned NOT NULL,
@@ -152,13 +77,6 @@ CREATE TABLE IF NOT EXISTS `order_item` (
   KEY `product_fk` (`product_idproduct`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `payment`
---
-
-DROP TABLE IF EXISTS `payment`;
 CREATE TABLE IF NOT EXISTS `payment` (
   `idpayment` bigint unsigned NOT NULL AUTO_INCREMENT,
   `user_iduser` bigint unsigned NOT NULL,
@@ -170,13 +88,6 @@ CREATE TABLE IF NOT EXISTS `payment` (
   KEY `user_iduser` (`user_iduser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `product`
---
-
-DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
   `idproduct` bigint unsigned NOT NULL AUTO_INCREMENT,
   `product_name` varchar(100) NOT NULL,
@@ -189,10 +100,6 @@ CREATE TABLE IF NOT EXISTS `product` (
   PRIMARY KEY (`idproduct`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
---
--- Daten für Tabelle `product`
---
-
 INSERT INTO `product` (`idproduct`, `product_name`, `product_category_name`, `price`, `short_description`, `long_description`, `active`, `date_added`) VALUES
 (1, 'Passivhaus', '', 300000.00, 'Haus mit U-Wert<10kWh/am2', 'Haus mit U-Wert<10kWh/am2\r\n20m2 Solaranlage\r\n40m2 Photovoltaik\r\n7500l Regenwassertank, ideal an kalten Wintertagen', 1, '2009-12-28 15:45:03'),
 (2, 'Niedrigenergiehaus', '', 250000.00, 'Haus mit U-Wert<45kWh/am2', 'Haus mit U-Wert<45kWh/am2\r\n20 m2 Solaranlage, ideal an kalten Wintertagen', 0, '2009-12-28 15:45:44'),
@@ -203,36 +110,17 @@ INSERT INTO `product` (`idproduct`, `product_name`, `product_category_name`, `pr
 ALTER TABLE product ADD FULLTEXT INDEX product_full_idx1 (product_name);
 ALTER TABLE product ADD FULLTEXT INDEX product_full_idx2 (product_name, short_description, long_description);
 
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `product_category`
---
-
-DROP TABLE IF EXISTS `product_category`;
 CREATE TABLE IF NOT EXISTS `product_category` (
   `idproduct_category` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `product_category_name` varchar(45) NOT NULL,
   PRIMARY KEY (`idproduct_category`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
---
--- Daten für Tabelle `product_category`
---
-
 INSERT INTO `product_category` (`idproduct_category`, `product_category_name`) VALUES
 (1, 'Grundstück'),
 (2, 'Haus'),
 (3, 'Wohnung');
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user`
---
-
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `iduser` bigint unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(100) NOT NULL,
@@ -249,22 +137,10 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`iduser`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
---
--- Daten für Tabelle `user`
--- Passwords have been generated with password_hash() of PHP. Both passwords are "geheim"
---
-
 INSERT INTO `user` (`iduser`, `first_name`, `last_name`, `nick_name`, `email`, `password`, `active`, `role`, `date_registered`, `phone`, `mobile`, `fax`) VALUES
 (1, 'shop', 'user1', 'shopuser1', 'shopuser1@onlineshop.at', '$2y$10$IO4DIPMzORQgxQz0cYi/1.RtEWeG5AYK90PsrAq1xKVINXDsyoivG', NULL, 'user', '2009-12-22 16:45:04', NULL, NULL, NULL),
 (2, 'shop', 'user2', 'shopuser2', 'shopuser2@onlineshop.at', '$2y$10$IO4DIPMzORQgxQz0cYi/1.RtEWeG5AYK90PsrAq1xKVINXDsyoivG', 'bdb678676c3f52999829403edc381449', 'user', '2009-12-28 15:52:43', NULL, NULL, NULL);
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `visit`
---
-
-DROP TABLE IF EXISTS `visit`;
 CREATE TABLE IF NOT EXISTS `visit` (
   `idvisit` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(45) NOT NULL,
@@ -274,10 +150,6 @@ CREATE TABLE IF NOT EXISTS `visit` (
   KEY `fk_visits_users1` (`user_iduser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Daten für Tabelle `visit`
---
-
 insert into visit ( `ip_address`, `timestamp`, `user_iduser`) values
 ('10.28.14.123', NOW(), 1),
 ('10.28.14.124', NOW(), 2),
@@ -285,48 +157,22 @@ insert into visit ( `ip_address`, `timestamp`, `user_iduser`) values
 ('10.28.14.123', NOW(), 2),
 ('10.28.14.125', NOW(), null);
 
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `address`
---
 ALTER TABLE `address`
   ADD CONSTRAINT `address_ibfk_2` FOREIGN KEY (`city_idcity`) REFERENCES `city` (`idcity`),
   ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`user_iduser`) REFERENCES `user` (`iduser`);
 
---
--- Constraints der Tabelle `city`
---
 ALTER TABLE `city`
   ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`country_idcountry`) REFERENCES `country` (`idcountry`);
 
---
--- Constraints der Tabelle `orders`
---
 ALTER TABLE `orders`
   ADD CONSTRAINT `user_fk` FOREIGN KEY (`user_iduser`) REFERENCES `user` (`iduser`);
 
---
--- Constraints der Tabelle `order_item`
---
 ALTER TABLE `order_item`
   ADD CONSTRAINT `product_fk` FOREIGN KEY (`product_idproduct`) REFERENCES `product` (`idproduct`),
   ADD CONSTRAINT `orders_fk` FOREIGN KEY (`orders_idorders`) REFERENCES `orders` (`idorders`);
 
---
--- Constraints der Tabelle `payment`
---
 ALTER TABLE `payment`
   ADD CONSTRAINT `fk_payment_user` FOREIGN KEY (`user_iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints der Tabelle `visits`
---
 ALTER TABLE `visit`
   ADD CONSTRAINT `fk_visits_users1` FOREIGN KEY (`user_iduser`) REFERENCES `user` (`iduser`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
