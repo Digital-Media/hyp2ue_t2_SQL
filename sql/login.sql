263fa9bb5eaa3c05f0314ea27fc020a2fe0912c8
martinharrer <martin.harrer@fh-hagenberg.at>
2022-01-21 12:14:01 +0100

    added GRANT to login.sql for Docker environment.

--- sql/login.sql
+++ sql/login.sql
@@ -4,6 +4,8 @@ DROP SCHEMA IF EXISTS login; from commandline
 Or PHPMyAdmin - databases - Operations to do this.
 PHPMyAdmin doesn't allow DROP DATABASE in a script or SQL-Window
  */
+-- Use GRANT-Statement, if you work with Docker, not necessary with Vagrant
+-- GRANT ALL PRIVILEGES ON *.* TO 'onlineshop'@'localhost';
 CREATE SCHEMA IF NOT EXISTS `login` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE login;
 -- place your solutions here
