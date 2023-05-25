cb581d0808de098997c02c6142e97bd3cee47a2b
Martin Harrer <martin.harrer@fh-hagenberg.at>
2023-05-25 06:27:01 +0200

    remove hyphens

--- sql/login.sql
+++ sql/login.sql
@@ -4,6 +4,8 @@ DROP SCHEMA IF EXISTS login; from commandline
 Or PHPMyAdmin - databases - Operations to do this.
 PHPMyAdmin doesn't allow DROP DATABASE in a script or SQL-Window
  */
-CREATE SCHEMA IF NOT EXISTS `login` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE SCHEMA IF NOT EXISTS login DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE login;
--- place your solutions here
+
+
+
